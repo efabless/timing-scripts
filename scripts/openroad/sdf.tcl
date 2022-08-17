80f521e09b2af11bd245c8c1fc87f13224d96063
Marwan Abbas <marwan@ciic.c.catx-ext-efabless.internal>
2022-08-17 13:52:01 -0700

    read all verilog files

--- scripts/openroad/sdf.tcl
+++ scripts/openroad/sdf.tcl
@@ -2,20 +2,26 @@ source $::env(TIMING_ROOT)/env/common.tcl
 source $::env(TIMING_ROOT)/env/$::env(LIB_CORNER).tcl
 
 set libs [split [regexp -all -inline {\S+} $libs]]
+set verilogs [split [regexp -all -inline {\S+} $verilogs]]
 
 foreach liberty $libs {
     read_liberty $liberty
 }
 
-set verilog $::env(CUP_ROOT)/verilog/gl/$::env(BLOCK).v
-if { ![file exists $verilog] } {
-    set verilog $::env(MCW_ROOT)/verilog/gl/$::env(BLOCK).v
-}
-if { ![file exists $verilog] } {
-    set verilog $::env(CARAVEL_ROOT)/verilog/gl/$::env(BLOCK).v
+foreach verilog $verilogs {
+    puts "reading veriolg: $verilog"
+    read_verilog $verilog
 }
 
-read_verilog $verilog
+# set verilog $::env(CUP_ROOT)/verilog/gl/$::env(BLOCK).v
+# if { ![file exists $verilog] } {
+#     set verilog $::env(MCW_ROOT)/verilog/gl/$::env(BLOCK).v
+# }
+# if { ![file exists $verilog] } {
+#     set verilog $::env(CARAVEL_ROOT)/verilog/gl/$::env(BLOCK).v
+# }
+# 
+# read_verilog $verilog
 
 link_design $block
 
