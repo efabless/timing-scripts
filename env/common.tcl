3d00f29be4cedfd95ca59affd7ce8c227e87b4bb
Kareem Farid <karimmhany@gmail.com>
2022-11-06 13:31:02 +0200

    feat: log the design and the time in run_puts_logs function

--- env/common.tcl
+++ env/common.tcl
@@ -113,6 +113,10 @@ proc run_puts {arg} {
 proc run_puts_logs {arg log} {
     set output [open "$log" w+]    
     puts $output "exec> $arg"
+    puts $output "design: $::env(BLOCK)"
+    set now [clock seconds]
+    set timestr [clock format $now -format "%y-%m-%d %H:%M:%S"]
+    puts $output "time: $timestr\n"
     close $output
     puts "exec> $arg >> $log"
     eval "{*}$arg >> $log"
