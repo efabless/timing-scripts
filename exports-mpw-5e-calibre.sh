054c6df3f6c7192e75719f34f7f53a5e747d55be
Kareem Farid <karimmhany@gmail.com>
2022-11-13 13:47:18 +0200

    chore: remove uneccesary exportst files

--- exports-mpw-5e-calibre.sh
+++
@@ -1,8 +0,0 @@
-export CARAVEL_ROOT=/home/kareem_farid/caravel-signoff/
-export MCW_ROOT=/home/kareem_farid/caravel-signoff/
-export CUP_ROOT=/home/kareem_farid/re-timing/cup-dummy
-export TIMING_ROOT=/home/kareem_farid/re-timing/timing-scripts
-export OPENLANE_IMAGE_NAME=efabless/openlane:4476a58407d670d251aa0be6a55e5391bb181c4e-amd64
-export PDK_ROOT=/home/kareem_farid/re-timing/pdk
-export LOGS_PREFIX=mpw-5e
-export SPEF_OVERWRITE='/home/kareem_farid/re-timing/mpw-5e/caravel/spef/caravel_$$::env(RCX_CORNER)_$(LIB_CORNER).spef'
