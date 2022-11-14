eff27284852756606c890494518f8666946a948e
Kareem Farid <karimmhany@gmail.com>
2022-11-14 18:24:47 +0200

    refactor: split generate_spef_mapping into multiple reusable files

--- scripts/generate_spef_mapping.py
+++ scripts/generate_spef_mapping.py
@@ -1,33 +1,12 @@
 from __future__ import absolute_import
 from __future__ import print_function
 from pathlib import Path
-from pyverilog.vparser.parser import parse
-from typing import List
+from verilog_parser import VerilogParser
+from pdk_helpers import get_macros, get_pdk_lefs_paths
 import click
 import logging
 import os
-import pyverilog
 import sys
-
-
-def get_pdk_lefs_paths(pdk_root: str, pdk: str) -> List[str]:
-    lef_paths = []
-    for root, dirs, files in os.walk(f"{pdk_root}/{pdk}"):
-        for file in files:
-            filename, file_extension = os.path.splitext(f"{file}")
-            if file_extension == ".lef":
-                lef_paths.append(f"{root}/{file}")
-    return lef_paths
-
-
-def get_macros(lef_file: str) -> List[str]:
-    macros = []
-    with open(lef_file) as f:
-        for line in f.readlines():
-            if "MACRO" in line:
-                macro_name = line.split()[1]
-                macros.append(macro_name)
-    return macros
 
 
 @click.command(
@@ -46,7 +25,7 @@ def get_macros(lef_file: str) -> List[str]:
 @click.option(
     "--project-root",
     required=True,
-    type=str,
+    type=click.Path(exists=True, file_okay=False),
     help="path of the project that will be used in the output spef mapping file",
 )
 @click.option(
@@ -79,40 +58,22 @@ def main(input, project_root, output, pdk_root, pdk, debug=False):
         pdk_macros = pdk_macros + get_macros(lef)
     logger.debug(pdk_macros)
 
-    if not os.path.isfile(input):
-        logger.error(f"netlist {input} not found")
-        exit(1)
-
-    files_list = [input]
     logger.info("parsing netlist..")
-    ast, _ = parse(files_list)
-    top_definition = None
-    instances = {}
-
-    for definition in ast.description.definitions:
-        def_type = type(definition).__name__
-        if def_type == "ModuleDef":
-            top_definition = definition
-
-    # Loop over each node under the top module definition
-    for item in top_definition.items:
-        item_type = type(item).__name__
-        if item_type == "InstanceList":  # Module instances
-            instance = item.instances[0]
-            instances[instance.name] = instance.module
-
-    logger.info("finding macros..")
-    # set SPEF_MAPPING_POSTFIX ".$::env(RCX_CORNER).spef"
-    # set SPEF_MAPPING_PREFIX "$::env(CARAVEL_ROOT)/signoff/gpio_control_block/openlane-signoff/spef/"
+    parsed = VerilogParser(input)
+    logger.info("comparing macros against pdk macros..")
     postfix = ".$::env(RCX_CORNER).spef"
     with open(output, "w") as f:
-        for instance in instances:
-            macro = instances[instance]
+        for instance in parsed.instances:
+            macro = parsed.instances[instance]
             if not (macro in pdk_macros):
                 logging.debug(f"{macro} not found in pdk_macros")
-                prefix = f"{project_root}/signoff/{macro}/openlane-signoff/spef/"
-                print(instance, macro)
-                f.write(f"set spef_mapping({instance}) {prefix}{macro}{postfix}\n")
+                spef_dir = "not-found"
+                for macro_spef_file in Path(project_root).rglob(f"{macro}*.spef"):
+                    spef_dir = macro_spef_file.parent
+                    break
+                
+                macro_spef = f"{spef_dir}/{macro}{postfix}"
+                f.write(f"set spef_mapping({instance}) \"{macro_spef}\"\n")
     logger.info(f"wrote to {output}")
 
 
