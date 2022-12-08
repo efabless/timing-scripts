584115437a03036f262e54de86dd0b4607db3809
Kareem Farid <karimmhany@gmail.com>
2022-12-08 20:42:33 +0200

    add ability to specify violation type in get_violations.py

--- scripts/get_violations.py
+++ scripts/get_violations.py
@@ -4,6 +4,7 @@ import click
 import logging
 import sys
 
+
 @click.command()
 @click.option(
     "--input",
@@ -12,51 +13,51 @@ import sys
     type=click.Path(exists=True, dir_okay=False),
     help="sta report",
 )
-@click.option(
-        "--append",
-        "-a",
-        is_flag=True
-        )
-def main(input, append):
+@click.option("--append", "-a", is_flag=True)
+@click.option("--type", required=True, type=click.Choice(["min", "max"]))
+def main(input, append, type):
     report = Report(input)
 
-
-    result = ""
+    result = f"{type}\n\n"
     violations = 0
     max_vio = 0
-    for path in report.flipflop_flipflop_paths:
-        if path.slack < 0:
-            violations += 1
-            max_vio = min(max_vio, path.slack)
-    result += (f"flipflop-flipflop: {violations}/{len(report.flipflop_flipflop_paths)}")
+    for path in report.reg_reg_paths:
+        if path.path_type == type:
+            if path.slack < 0:
+                violations += 1
+                max_vio = min(max_vio, path.slack)
+    result += f"reg-reg: {violations}/{len(report.reg_reg_paths)}"
     result += "\n"
 
     violations = 0
-    for path in report.flipflop_output_paths:
-        if path.slack < 0:
-            violations += 1
-    result += (f"flipflop-output: {violations}/{len(report.flipflop_output_paths)}")
+    for path in report.reg_output_paths:
+        if path.path_type == type:
+            if path.slack < 0:
+                violations += 1
+    result += f"reg-output: {violations}/{len(report.reg_output_paths)}"
     result += "\n"
 
     violations = 0
-    for path in report.input_flipflop_paths:
-        if path.slack < 0:
-            violations += 1
-    result += (f"input-flipflop: {violations}/{len(report.input_flipflop_paths)}")
+    for path in report.input_reg_paths:
+        if path.path_type == type:
+            if path.slack < 0:
+                violations += 1
+    result += f"input-reg: {violations}/{len(report.input_reg_paths)}"
     result += "\n"
 
     violations = 0
     for path in report.input_output_paths:
         if path.slack < 0:
             violations += 1
-    result += (f"input-output: {violations}/{len(report.input_output_paths)}")
+    result += f"input-output: {violations}/{len(report.input_output_paths)}"
     result += "\n"
 
     violations = 0
     for path in report.unknown_paths:
-        if path.slack < 0:
-            violations += 1
-    result += (f"unkown: {violations}/{len(report.unknown_paths)}")
+        if path.path_type == type:
+            if path.slack < 0:
+                violations += 1
+    result += f"unkown: {violations}/{len(report.unknown_paths)}"
     result += "\n"
 
     if append:
@@ -65,7 +66,6 @@ def main(input, append):
     else:
         print(result)
     print(round(max_vio, 2))
-
 
 
 if __name__ == "__main__":
