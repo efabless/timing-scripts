255dc402f3d01ad0577a1822810f1bb4287f8fdc
Passant <bassant.hassan@efabless.com>
2023-10-18 14:12:40 +0300

    add argument to `get_violations.py` to choose the output worst slack category

--- scripts/get_violations.py
+++ scripts/get_violations.py
@@ -11,11 +11,12 @@ def filter_violating(paths):
 @click.command()
 @click.option("--append", "-a", is_flag=True)
 @click.option("--type", required=True, type=click.Choice(["min", "max"]))
+@click.option("--category", type=click.Choice(["reg-reg", "reg-output", "input-reg", "input-output"]), default="reg-reg")
 @click.argument(
     "input",
     type=click.Path(exists=True, dir_okay=False),
 )
-def main(input, append, type):
+def main(input, append, type, category):
     report = Report(input)
     max_vio = 0
     paths_dict = {
@@ -26,12 +27,12 @@ def main(input, append, type):
     }
     result = f"{type}:\n"
 
-    for category, paths in paths_dict.items():
+    for sp_ep_pair, paths in paths_dict.items():
         filtered_paths = filter_paths(paths, type)
         violating_paths = filter_violating(filtered_paths)
-        if violating_paths != [] and category == "reg-reg":
+        if violating_paths != [] and sp_ep_pair == category:
             max_vio = min(min(violating_paths, key=lambda x: x.slack).slack, max_vio)
-        result += f"{category}: {len(violating_paths)}/{len(filtered_paths)}\n"
+        result += f"{sp_ep_pair}: {len(violating_paths)}/{len(filtered_paths)}\n"
 
     if append:
         with open(input, "a") as stream:
