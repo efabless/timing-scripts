04a20676978e55e5d3dc9ab97e0da88ef6c06456
Passant <bassant.hassan@efabless.com>
2023-10-18 15:09:31 +0300

    add caravel spef mapping in PDK configuration add all sram ips lefs

--- env/gf180mcuC/config.tcl
+++ env/gf180mcuC/config.tcl
@@ -6,15 +6,22 @@ set io_library              "gf180mcu_fd_io"
 set tech_lef $::env(PDK_REF_PATH)/$std_cell_library/techlef/${std_cell_library}__$::env(RCX_CORNER).tlef
 set cells_lef $::env(PDK_REF_PATH)/$std_cell_library/lef/$std_cell_library.lef
 set io_lef $::env(PDK_REF_PATH)/$io_library/lef/$io_library.lef
-set sram_lef $::env(PDK_REF_PATH)/gf180mcu_fd_ip_sram/lef/gf180mcu_fd_ip_sram__sram512x8m8wm1.lef
+set sram_lefs [list \
+    $::env(PDK_REF_PATH)/gf180mcu_fd_ip_sram/lef/gf180mcu_fd_ip_sram__sram128x8m8wm1.lef \
+    $::env(PDK_REF_PATH)/gf180mcu_fd_ip_sram/lef/gf180mcu_fd_ip_sram__sram256x8m8wm1.lef \
+    $::env(PDK_REF_PATH)/gf180mcu_fd_ip_sram/lef/gf180mcu_fd_ip_sram__sram512x8m8wm1.lef \
+    $::env(PDK_REF_PATH)/gf180mcu_fd_ip_sram/lef/gf180mcu_fd_ip_sram__sram64x8m8wm1.lef
+]
+
 
 set pdk(lefs) [list \
     $tech_lef \
     $cells_lef \
     $io_lef \
-    $sram_lef
+    $sram_lefs
 ]
 
 set pdk(rcx_rules_file) $::env(PDK_TECH_PATH)/openlane/rules.openrcx.$::env(PDK).$::env(RCX_CORNER)
 
 source $::env(TIMING_ROOT)/env/$::env(PDK)/$::env(LIB_CORNER).tcl
+source $::env(TIMING_ROOT)/env/caravel_spef_mapping-gfmpw.tcl
