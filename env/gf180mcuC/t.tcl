c0f95a617f2100630a82bf3b4adb1216e1b38280
Kareem Farid <kareefardi@users.noreply.github.com>
2023-10-18 16:58:32 +0300

    gf180mcu support (#18)
    + add configurations for `gf180mcuC` and `gf180mcuD`
    + add spef mapping script for caravel gfmpw

---
+++ env/gf180mcuC/t.tcl
@@ -0,0 +1,9 @@
+set pdk(libs) [list \
+$::env(PDK_REF_PATH)/${std_cell_library}/liberty/${std_cell_library}__tt_025C_5v00.lib \
+$::env(PDK_REF_PATH)/${io_library}/liberty/gf180mcu_fd_io__tt_025C_5v00.lib \
+$::env(PDK_REF_PATH)/gf180mcu_fd_ip_sram/liberty/gf180mcu_fd_ip_sram__sram128x8m8wm1__tt_025C_5v00.lib \
+$::env(PDK_REF_PATH)/gf180mcu_fd_ip_sram/liberty/gf180mcu_fd_ip_sram__sram256x8m8wm1__tt_025C_5v00.lib \
+$::env(PDK_REF_PATH)/gf180mcu_fd_ip_sram/liberty/gf180mcu_fd_ip_sram__sram512x8m8wm1__tt_025C_5v00.lib \
+$::env(PDK_REF_PATH)/gf180mcu_fd_ip_sram/liberty/gf180mcu_fd_ip_sram__sram64x8m8wm1__tt_025C_5v00.lib \
+]
+
---
+++ env/gf180mcuD/config.tcl
@@ -0,0 +1,26 @@
+set std_cell_library        "gf180mcu_fd_sc_mcu7t5v0"
+set io_library              "gf180mcu_fd_io"
+
+
+
+set tech_lef $::env(PDK_REF_PATH)/$std_cell_library/techlef/${std_cell_library}__$::env(RCX_CORNER).tlef
+set cells_lef $::env(PDK_REF_PATH)/$std_cell_library/lef/$std_cell_library.lef
+set io_lef $::env(PDK_REF_PATH)/$io_library/lef/$io_library.lef
+set sram_lefs [list \
+    $::env(PDK_REF_PATH)/gf180mcu_fd_ip_sram/lef/gf180mcu_fd_ip_sram__sram128x8m8wm1.lef \
+    $::env(PDK_REF_PATH)/gf180mcu_fd_ip_sram/lef/gf180mcu_fd_ip_sram__sram256x8m8wm1.lef \
+    $::env(PDK_REF_PATH)/gf180mcu_fd_ip_sram/lef/gf180mcu_fd_ip_sram__sram512x8m8wm1.lef \
+    $::env(PDK_REF_PATH)/gf180mcu_fd_ip_sram/lef/gf180mcu_fd_ip_sram__sram64x8m8wm1.lef
+]
+
+set pdk(lefs) [list \
+    $tech_lef \
+    $cells_lef \
+    $io_lef \
+    $sram_lefs
+]
+
+set pdk(rcx_rules_file) $::env(PDK_TECH_PATH)/openlane/rules.openrcx.$::env(PDK).$::env(RCX_CORNER)
+
+source $::env(TIMING_ROOT)/env/$::env(PDK)/$::env(LIB_CORNER).tcl
+source $::env(TIMING_ROOT)/env/caravel_spef_mapping-gfmpw.tcl
